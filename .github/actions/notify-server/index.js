9c45408aab561f27eae44d37d46d12a051fad13b
anbudamo <anbu.damodar@gmail.com>
2025-09-01 18:03:41 -0400

    added custom github action that contacts local web server
    local github action in .github/actions/notify-server, runs js script that contacts a local express server

---
+++ .github/actions/notify-server/index.js
@@ -0,0 +1,21 @@
+const core = require('@actions/core');
+const axios = require('axios')
+
+async function run() {
+    try{
+        const url = core.getInput('url')
+        const token = core.getInput('token')
+
+        await axios.post(url, { message: 'Workflow completed' }, {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log('Notif sent to local server');
+    } catch (error) {
+        core.setFailed(`Action failed with error: ${error.message}`);
+    }
+}
+
+run()
