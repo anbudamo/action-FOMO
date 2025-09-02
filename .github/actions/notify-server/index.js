100dd6b439954f74b42c4d1767f8425d18338c18
Anbu Damodaran <109062338+anbudamo@users.noreply.github.com>
2025-09-01 21:38:49 -0400

    typo: status

--- .github/actions/notify-server/index.js
+++ .github/actions/notify-server/index.js
@@ -6,16 +6,16 @@ async function run() {
         const url = core.getInput('url')
         const token = core.getInput('token')
 
-        response = await axios.post(url, { message: 'Workflow completed' }, {
+        const response = await axios.post(url, { message: 'Workflow completed' }, {
             headers: {
                 'Authorization': `Bearer ${token}`,
                 'Content-Type': 'application/json'
             }
-        }).then(function (response) {
-            console.log('Response status: ', response.status)
         })
-        console.log('Notif sent to local server');
-    } catch (error) {response.status)
+        console.log('Response status: ', response.status)
+        console.log('Notif sent to local server')
+        
+    } catch (error) {
         core.setFailed(`Action failed with error: ${error.message}`)
     }
 }
